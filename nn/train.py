c429fdc928cdc5d85229619239c6690fe00c6620
Cameron Fabbri <cameronfabbri@gmail.com>
2018-04-17 16:05:05 -0500

    update

--- nn/train.py
+++ nn/train.py
@@ -9,6 +9,11 @@ import requests
 import pickle
 import gzip
 import os
+import sklearn
+
+from sklearn import datasets
+
+import matplotlib.pyplot as plt
 
 # import my nn class
 from network import neuralNetwork as nn
@@ -39,21 +44,28 @@ if __name__ == '__main__':
    #network = nn(784, 10)
 
    # dummy data
-   data = np.asarray([[0,1], [1,0], [1,1], [0,0]])
-   labels = np.asarray([[1], [1], [0], [0]])
+   #data = np.asarray([[0,1], [1,0], [1,1], [0,0]])
+   #labels = np.asarray([[1], [1], [0], [0]])
+   
+   X, y = sklearn.datasets.make_moons(200, noise=0.20)
+   print X.shape
+   print X[0].shape
+   exit()
+   #plt.scatter(X[:,0], X[:,1], s=40, c=y, cmap=plt.cm.Spectral)
    
    network = nn()
 
    # define the layers
    input_layer   = Layer(2, input_=True, name='input_layer')
    hidden_layer1 = Layer(3, previous_layer=input_layer, name='hidden_layer1')
-   output_layer  = Layer(1, previous_layer=hidden_layer1, output_=True, name='output_layer')
+   output_layer  = Layer(2, previous_layer=hidden_layer1, output_=True, name='output_layer')
 
    # add layers to the network
    network.addLayer(input_layer)
    network.addLayer(hidden_layer)
    network.addLayer(output_layer)
 
+   
 
 
 
