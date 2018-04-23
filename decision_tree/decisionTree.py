3cd5b802ced1a19f7cf12a981643c6cd157b89c5
Cameron Fabbri <cameronfabbri@gmail.com>
2018-04-23 15:37:28 -0500

    update

---
+++ decision_tree/decisionTree.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+'''
+
+   Class for a decision tree
+
+'''
+class decisionTree(object):
+
+   def __init__(self, depth=4):
+      self.depth = depth
+
+
+class node(object):
+
+   def __init__(self):
+      self.value = ''
+
+   
+
+
+d = decisionTree()
+
+print d.depth
+
